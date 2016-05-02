317be7b7df504df7cb5da3eb3be6f3846b51487d
Javier Moreno Sanchez <javiermsm12@gmail.com>
2016-05-02 20:46:34 +0200

    Crear preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,55 +1,62 @@
 
 var models = require('../models');
 
+//Autoload el quiz asociado a :quizId
+exports.load = function(req, res, next, quizId) {
+	models.Quiz.findById(quizId)
+		.then(function(quiz) {
+			if (quiz) {
+				req.quiz = quiz;
+				next();
+			} else{
+				next(new Error('No existe quizId=' + quizId));
+			}
+		}).catch(function(error) { next(error); });
+};
 
 // GET /quizzes
 exports.index = function(req, res, next) {
 	models.Quiz.findAll()
 		.then(function(quizzes) {
 			res.render('quizzes/index.ejs', { quizzes: quizzes});
-		})
-		.catch(function(error) {
-			next(error);
-		});
+		}).catch(function(error) { next(error); });
 };
 
 
 // GET /quizzes/:id
 exports.show = function(req, res, next) {
-	models.Quiz.findById(req.params.quizId)
-		.then(function(quiz) {
-			if (quiz) {
-				var answer = req.query.answer || '';
-
-				res.render('quizzes/show', {quiz: quiz,
-											answer: answer});
-			} else {
-		    	throw new Error('No existe ese quiz en la BBDD.');
-		    }
-		})
-		.catch(function(error) {
-			next(error);
-		});
+	var answer = req.query.answer || '';
+	res.render('quizzes/show', {quiz: req.quiz,
+								answer: answer});
 };
 
 
 // GET /quizzes/:id/check
 exports.check = function(req, res) {
-	models.Quiz.findById(req.params.quizId)
+	var answer = req.query.answer || '';
+	var result = answer === req.quiz.answer ? 'Correcta' : 'Incorrecta';
+	res.render('quizzes/result', {quiz: req.quiz,
+									result: result,
+									answer: answer});
+};
+
+//GET /quizzes/new
+exports.new = function(req, res, next) {
+	var quiz = models.Quiz.build({question: "", answer: ""});
+	res.render('quizzes/new', {quiz: quiz});
+};
+
+//POST /quizzes/create
+exports.create = function(req, res, next) {
+	var quiz = models.Quiz.build({ question: req.body.quiz.question,
+								   answer:   req.body.quiz.answer} );
+
+//guarda en DB los campos pregunta y respuesta de quiz
+	quiz.save({fields: ["question", "answer"]})
 		.then(function(quiz) {
-			if (quiz) {
-				var answer = req.query.answer || "";
-
-				var result = answer === quiz.answer ? 'Correcta' : 'Incorrecta';
-
-				res.render('quizzes/result', { quiz: quiz, 
-											   result: result, 
-											   answer: answer });
-			} else {
-				throw new Error('No existe ese quiz en la BBDD.');
-			}
+			res.redirect('/quizzes'); // res.redirect: Redireccion HTTP a lista de preguntas
 		})
 		.catch(function(error) {
 			next(error);
-		});	
+		});
 };
