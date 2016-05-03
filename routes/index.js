eb99ed9c90729a34866c6c4064830c10fbffa6f9
Javier Moreno Sanchez <javiermsm12@gmail.com>
2016-05-03 17:16:39 +0200

    Buscar pregunta

--- routes/index.js
+++ routes/index.js
@@ -12,11 +12,12 @@ router.get('/', function(req, res, next) {
 router.param('quizId', quizController.load); //autoload :quizId
 
 // Definición de rutas de /quizzes
-router.get('/quizzes',                     quizController.index);
-router.get('/quizzes/:quizId(\\d+)',       quizController.show);
-router.get('/quizzes/:quizId(\\d+)/check', quizController.check);
-router.get('/quizzes/new',                 quizController.new);
-router.post('/quizzes',                    quizController.create);
+router.get('/quizzes',                      quizController.index);
+router.get('/quizzes/:quizId(\\d+)',        quizController.show);
+router.get('/quizzes/:quizId(\\d+)/check',  quizController.check);
+router.get('/search', 						quizController.search);
+router.get('/quizzes/new',                  quizController.new);
+router.post('/quizzes',                     quizController.create);
 
 
 module.exports = router;
