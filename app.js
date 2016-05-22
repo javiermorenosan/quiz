000d0559c284923e2cb9fa59be3bd3a30143d1d1
Javier Moreno Sanchez <javiermsm12@gmail.com>
2016-05-22 11:43:48 +0200

    Mensajes Flash

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var session = require('express-session');
 var partials = require('express-partials');
-
+var flash = require('express-flash');
 var routes = require('./routes/index');
 
 var app = express();
@@ -14,17 +14,18 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
-app.use(partials());
-
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(session({secret: "Quiz 2016",
+                  resave: false, 
+                  saveUninitialized: true}));
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(partials());
+app.use(flash());
 app.use('/', routes);
 
 // catch 404 and forward to error handler
