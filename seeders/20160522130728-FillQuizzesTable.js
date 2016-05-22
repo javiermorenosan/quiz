eb6c960afaa33651e14fb96961614ebef135b7b1
Javier Moreno Sanchez <javiermsm12@gmail.com>
2016-05-22 17:51:17 +0200

    Migración local

--- seeders/20160522130728-FillQuizzesTable.js
+++ seeders/20160522130728-FillQuizzesTable.js
@@ -2,10 +2,14 @@
 
 module.exports = {
   up: function (queryInterface, Sequelize) {
-    return queryInterface.bulkInsert('Quizzes', [
-      { question: 'Capital de Italia', answer: 'Roma', createdAt: new Date(), updatedAt: new Date()}, 
-        question: 'Capital de Portugal', answer: 'Lisboa', createdAt: new Date(), updatedAt: new Date()}, 
-        question: 'Capital de España', answer: 'Madrid', createdAt: new Date(), updatedAt: new Date()}, 
+
+      return queryInterface.bulkInsert('Quizzes', [ 
+         { question: 'Capital de Italia', answer: 'Roma',
+           createdAt: new Date(), updatedAt: new Date() },
+         { question: 'Capital de Portugal', answer: 'Lisboa',
+           createdAt: new Date(), updatedAt: new Date() },
+         { question: 'Capital de España', answer: 'Madrid',
+           createdAt: new Date(), updatedAt: new Date() },
         ]);
   },
 
