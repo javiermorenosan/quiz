254e6af5714178ea98e4a0f0611f1baa5b9ff868
Javier Moreno Sanchez <javiermsm12@gmail.com>
2016-05-22 13:23:26 +0200

    Migraciones y seeders

--- models/index.js
+++ models/index.js
@@ -27,25 +27,27 @@ var sequelize = new Sequelize(url,
 
 //Importar la definición de la tabla Quiz de quiz.js
 var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
-//sequelize.sync() crea e inicializa tabla de preguntas en DB
-sequelize
-.sync()
-.then(function() { // Ya se han creado las tablas necesarias.
-	return Quiz.count()
-			.then(function (c) {
-				if (c === 0) { //la tabla se inicializa solo si esta vacía
-					return Quiz
-							.bulkCreate([ {question: 'Capital de Italia', answer: 'Roma'},
-										  {question: 'Capital de Portugal', answer: 'Lisboa'}
-										])
-							.then(function() {
-								console.log('Base de datos inicializada con datos');
-							});
-				}
-			});
-})
-.catch(function(error){
-	console.log("Error sincronizando las tablas de la BBDD: ", error);
-	process.exit(1);
-});
+
+
+////sequelize.sync() crea e inicializa tabla de preguntas en DB
+//sequelize
+//.sync()
+//.then(function() { // Ya se han creado las tablas necesarias.
+//	return Quiz.count()
+//			.then(function (c) {
+//				if (c === 0) { //la tabla se inicializa solo si esta vacía
+//					return Quiz
+//							.bulkCreate([ {question: 'Capital de Italia', answer: 'Roma'},
+//										  {question: 'Capital de Portugal', answer: 'Lisboa'}
+//										])
+//							.then(function() {
+//								console.log('Base de datos inicializada con datos');
+//							});
+//				}
+//			});
+//})
+//.catch(function(error){
+//	console.log("Error sincronizando las tablas de la BBDD: ", error);
+//	process.exit(1);
+//});
 exports.Quiz = Quiz; //exportar definicion de la tabla Quiz
